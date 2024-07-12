672053b965bb02572e0602412a92c47634c47268
DD <dilip.dakshin@gmail.com>
2024-07-12 17:01:53 +0530

    Tasks models created

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    PRIORITY_CHOICES = [
+        ('L', 'Low'),
+        ('M', 'Medium'),
+        ('H', 'High'),
+    ]
+
+    STATUS_CHOICES = [
+        ('P', 'Pending'),
+        ('I', 'In Progress'),
+        ('C', 'Completed')
+    ]
+
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    priority = models.CharField(max_length=1, choices=PRIORITY_CHOICES, default='M')
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default='P')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
