528859f310744d0f2cc0cc06ed912caa301de580
DD <dilip.dakshin@gmail.com>
2024-07-12 16:52:08 +0530

    Dockerization completed

--- taskwave/settings.py
+++ taskwave/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'taskwave.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'taskwave_dev',
+        'USER': 'task_admin',
+        'PASSWORD': 'SystemTask123',
+        'HOST': 'db',
+        'PORT': '5432'
     }
 }
 
